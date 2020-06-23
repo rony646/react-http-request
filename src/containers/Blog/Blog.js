13570a3fa8a7eb7773d683dc8a2468c612df8377
rony646 <rony646@users.noreply.github.com>
2020-06-23 17:00:12 -0300

    axios get request

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,36 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                const posts = response.data.slice(0,4);
+                const updatePosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Rony Silva'
+                    }
+                })
+                this.setState({posts: updatePosts});
+            })
+    }
+
+
     render () {
+        const posts = this.state.posts.map(item => {
+            return <Post key={item.id} title={item.title} author={item.author}/>
+        })
+        
+        console.log(this.state.posts)
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
