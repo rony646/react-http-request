7c499f5c157fded6a40be53c773966abed995782
rony646 <rony646@users.noreply.github.com>
2020-06-26 14:08:31 -0300

    axiso defaults

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -13,7 +13,7 @@ class FullPost extends Component {
         if(this.props.id) {
             if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
 
-                axios.get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+                axios.get(`/posts/${this.props.id}`)
                         .then(response => {
                         this.setState({loadedPost: response.data})
                     })
@@ -22,7 +22,7 @@ class FullPost extends Component {
     }
 
     deleteHandler = () => {
-        axios.delete(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+        axios.delete(`/posts/${this.props.id}`)
             .then(res => {
                 console.log(res) // It wasn't really deleted
             })
--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -18,7 +18,7 @@ class NewPost extends Component {
             author: this.state.author
         }
 
-        axios.post('https://jsonplaceholder.typicode.com/posts', data)
+        axios.post('/posts', data)
             .then(response => {
                 console.log(response)
                 this.setState({postSent: true})
