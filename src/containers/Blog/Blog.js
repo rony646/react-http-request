db74713065a37b0f238a2db8a0fda043e5f6be78
rony646 <rony646@users.noreply.github.com>
2020-07-06 11:40:38 -0300

    setting up links

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -53,7 +53,15 @@ class Blog extends Component {
         console.log(posts, this.state.error)
         
         return (
-            <div>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
                     {posts}
                 </section>
