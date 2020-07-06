812c5abc2af876d4e331e344519edab10b1fd68c
rony646 <rony646@users.noreply.github.com>
2020-07-06 16:34:50 -0300

    started routing configuration

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+import { Route } from 'react-router-dom'
+
 
 import './Blog.css';
 import Posts from './Posts/Posts'
+import NewPost from '../../containers/Blog/NewPost/NewPost'
 
 class Blog extends Component {
     render () {
@@ -16,7 +18,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                <Route path="/" exact component={Posts}/> {/* Render the "Posts" component in the root path*/}
+                <Route path="/new-post" exact component={NewPost} />
             </div>
         );
     }
